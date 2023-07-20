4452323d155623f6496d29b473e8241aed0825dd
Konstantin Loktiev <81988686+Kosar0111@users.noreply.github.com>
2023-07-20 20:39:23 +0300

    1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Header } from './components';
+import { Cart, Home } from './pages';
+import { Route } from 'react-router-dom';
+import axios from 'axios'
+/* import { createStore } from 'redux' */
+/* import { useSelector, useDispatch } from 'react-redux'; */
+/* import { setPizzas } from './redux/actions/pizzas'; */
 
 function App() {
+
+  const [pizzas, setPizzas] = React.useState([])
+
+  React.useEffect(() => {
+    axios.get('http://localhost:3000/db.json').then(({ data }) => {
+      setPizzas(data.pizzas);
+    });
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+      <Header />
+      <div className="content">
+
+        <Route path='/' render={() => <Home items={pizzas} />} exact />
+        <Route path='/cart' component={Cart} exact />
+
+      </div>
     </div>
   );
 }
 
+/*   fetch('http://localhost:3000/db.json').then((resp) =>
+    resp.json()).then(json => {
+      setPizzas(json.pizzas)
+    });
+}, []) 
+
+*/
+/* /* class App extends React.Component { */
+/*
+
+  (state) => {
+    return {
+      items: state.pizzas.items,
+      filters: state.filters,
+    }
+  },
+  (dispatch) => {
+    return {
+      setPizzas: (items) => dispatch(setPizzas(items))
+    }
+  }
+export default connect()(App) */
 export default App;
